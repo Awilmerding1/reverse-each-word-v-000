9ccc1ba0ce52c5bfd4450d1aa5ef19864a7a6660
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-09 15:16:58 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   back = sentence.split(" ")
   sentence_array = []
   back.each do |word|
-    sentence_array <<  word.reverse 
+    sentence_array <<  "#{word.reverse}"
    return sentence_array.join(" ")
 end  
 end
